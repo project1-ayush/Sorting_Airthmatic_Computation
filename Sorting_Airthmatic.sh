8c41f1bb6b2b50c8a69f811627c6e9871913024d
project1-ayush <trivediayush491112@gmail.com>
2021-03-28 12:26:43 +0530

    Added: Array in Descending Order

--- Sorting_Airthmatic.sh
+++ Sorting_Airthmatic.sh
@@ -1,6 +1,7 @@
 #|/bin/bash -x
 
 count=0
+temp=0
 
 declare -a Results
 
@@ -32,3 +33,18 @@ Results[((count++))]=$result4
 
 echo "Result Of Airthmatic Problem" ${Results[@]}
 echo "Index Of Result Of Airthmatic Problem" ${!Results[@]}
+
+for (( i=0;i<5;i++ ))
+do
+		for (( j=0;j<5-i-1;j++ ))
+		do
+			if [ ${Results[j]} -lt ${Results[$((j+1))]} ]
+				then
+					temp=${Results[j]}
+					Results[$j]=${Results[$((j+1))]}
+					Results[$((j+1))]=$temp
+			 fi
+		done
+done
+
+echo "Results in Descending Order ${Results[@]}"
