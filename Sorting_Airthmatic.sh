d5d525050228b2d4cdc94e3735d4c3d9261f2539
project1-ayush <trivediayush491112@gmail.com>
2021-03-28 12:30:43 +0530

    Added: Array in Ascending Order

--- Sorting_Airthmatic.sh
+++ Sorting_Airthmatic.sh
@@ -38,7 +38,7 @@ for (( i=0;i<5;i++ ))
 do
 		for (( j=0;j<5-i-1;j++ ))
 		do
-			if [ ${Results[j]} -lt ${Results[$((j+1))]} ]
+			if [ ${Results[j]} -gt ${Results[$((j+1))]} ]
 				then
 					temp=${Results[j]}
 					Results[$j]=${Results[$((j+1))]}
